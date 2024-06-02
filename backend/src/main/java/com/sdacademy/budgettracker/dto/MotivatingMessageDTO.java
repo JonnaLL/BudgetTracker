a3a7a3b5720e15acc699293dc2196bfa63e5c6cb
merilipokasaar <Merilipokasaar@gmail.com>
2024-06-02 11:12:58 +0300

    corrections and added motivatiing message dto part

---
+++ backend/src/main/java/com/sdacademy/budgettracker/dto/MotivatingMessageDTO.java
@@ -0,0 +1,12 @@
+package com.sdacademy.budgettracker.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MotivatingMessageDTO {
+    private Long id;
+    private String message;
+    private String author;
+}
