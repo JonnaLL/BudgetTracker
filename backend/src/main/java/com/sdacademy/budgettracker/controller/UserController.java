32ef896c73c0dbb51e2506c67980fed94a94d6ed
JonnaLL <jonnaliisa@hotmail.com>
2024-06-08 13:19:41 +0300

    Backend files to fix dashboard

--- backend/src/main/java/com/sdacademy/budgettracker/controller/UserController.java
+++ backend/src/main/java/com/sdacademy/budgettracker/controller/UserController.java
@@ -1,2 +1,25 @@
-package com.sdacademy.budgettracker.controller;public class UserController {
+package com.sdacademy.budgettracker.controller;
+
+import com.sdacademy.budgettracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{userId}/username")
+    public ResponseEntity<String> getUsername(@PathVariable Long userId) {
+        try {
+            String username = userService.findUsernameById(userId);
+            return ResponseEntity.ok(username);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to get username: " + e.getMessage());
+        }
+    }
 }
