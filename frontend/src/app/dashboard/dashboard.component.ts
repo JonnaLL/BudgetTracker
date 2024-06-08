76074b830a195e529bb2705d83f7cd564af30f17
JonnaLL <jonnaliisa@hotmail.com>
2024-06-08 13:07:34 +0300

    dashboard changes

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { MotivationModalComponent } from '../motivation-modal/motivation-modal.component';
 import { BudgetService } from '../../services/budget.service';
@@ -17,7 +17,6 @@ export class DashboardComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private route: ActivatedRoute,
     private dialog: MatDialog,
     private budgetService: BudgetService,
     private router: Router
@@ -27,22 +26,22 @@ export class DashboardComponent implements OnInit {
     const storedUserId = localStorage.getItem('userId');
     if (storedUserId) {
       this.userId = Number(storedUserId);
-      this.fetchUserData();
-      this.openMotivationModal();  
-      
+      this.fetchUsername();
     } else {
       this.router.navigate(['/home']);
     }
   }
 
-  fetchUserData() {
+  fetchUsername() {
     if (this.userId !== null) {
-      this.userService.getUserData(this.userId).subscribe(userData => {
-        this.userInfo = userData;
-        this.welcomeMessage = `Hi, ${userData.username}!`;
-      }, error => {
-        console.error('Error fetching user data:', error);
-      });
+      this.userService.getUsername(this.userId).subscribe(
+        username => {
+          this.welcomeMessage = `Hello, ${username}!`;
+        },
+        error => {
+          console.error('Error fetching username:', error);
+        }
+      );
     }
   }
 
